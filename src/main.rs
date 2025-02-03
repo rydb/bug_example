9ac74f0ce8b7d66752b187cd0ddc7211fa2de607
rydb <ryan.dobe@gmail.com>
2025-02-03 11:39:00 -0600

    added comments

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,37 @@
 use bevy::{gltf::GltfPrimitive, prelude::*};
 
-
-
 fn main() {
     App::new()
-    .init_resource::<PrimitiveHandle>()
-
-    .add_plugins(DefaultPlugins)
-    .add_systems(Startup, setup)
-    .add_systems(Update, check_load_status)
-    .run();
+        .init_resource::<PrimitiveHandle>()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, check_load_status)
+        .run();
 }
 
 #[derive(Resource, Default)]
 pub struct PrimitiveHandle(Option<Handle<GltfPrimitive>>);
 
 /// check load status of asset at different stages.
-pub fn check_load_status(
-    asset_server: Res<AssetServer>,
-    mut primitive: ResMut<PrimitiveHandle>,
-) {
-    //match asset_server.load_state(node.0)
+pub fn check_load_status(asset_server: Res<AssetServer>, mut primitive: ResMut<PrimitiveHandle>) {
     if let Some(handle) = &primitive.0 {
-        let path =  handle.path().unwrap();
+        let path = handle.path().unwrap();
         match asset_server.load_state(handle) {
-            bevy::asset::LoadState::NotLoaded => println!("not loaded {:#}", path ),
+            bevy::asset::LoadState::NotLoaded => println!("not loaded {:#}", path),
+            // perpetually stuck laoding here, but no error from the asset loader about being an invalid asset?
             bevy::asset::LoadState::Loading => {
                 println!("loading {:#?}", path)
-                
-            },
+            }
             bevy::asset::LoadState::Loaded => {
                 println!("loaded {:#}", path);
                 primitive.0 = None;
-            },
-            bevy::asset::LoadState::Failed(asset_load_error) => println!("failed to load {:#}. REASON: {:#}", path, asset_load_error),
+            }
+            bevy::asset::LoadState::Failed(asset_load_error) => {
+                println!("failed to load {:#}. REASON: {:#}", path, asset_load_error)
+            }
         }
     }
 }
-
 
 /// set up a simple 3D scene
 fn setup(
@@ -47,7 +41,9 @@ fn setup(
     mut primitive: ResMut<PrimitiveHandle>,
     asset_server: Res<AssetServer>,
 ) {
+    // example primitive.
     primitive.0 = Some(asset_server.load("cube.glb#Mesh0/Primitive0"));
+
     // circular base
     commands.spawn((
         Mesh3d(meshes.add(Circle::new(4.0))),
